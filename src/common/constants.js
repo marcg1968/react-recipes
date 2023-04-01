1c59629d2f294429df6cfc0999abbb46d7f765b5
Marc Greyling <marc@cubi201.marcjan.de>
2023-04-02 09:29:07 +1000

    yarn add axios styled-components

---
+++ src/common/constants.js
@@ -0,0 +1,8 @@
+// constants.js
+
+export const baseApiUrl = 'https://recipes.greyling.info/api/v2'
+
+export const ApiEndpoint = {
+    recipes: `${baseApiUrl}/recipes`,
+    recipe: `${baseApiUrl}/recipe`,
+}
